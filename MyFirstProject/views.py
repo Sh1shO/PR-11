025f5a5a7412ae8a888ed8d482f66c461d5bbc37
student-pro <student-pro@b206-9.khpk.ru>
2024-04-06 12:51:36 +0700

    first commit

---
+++ MyFirstProject/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import sqlite3
+
+# Create your views here.
+def index(request):
+    flag = False
+    if request.method == "POST":
+        login = request.POST.get("login", None)
+        password = request.POST.get("password", None)
+        connection = sqlite3.connect('identifier.sqlite')
+        cursor = connection.cursor()
+        select_sql = "SELECT * FROM users WHERE login = '{}' and password = '{}';".format(login, password)
+        cursor = cursor.execute(select_sql)
+        for row in cursor:
+            if row[0] == login:
+                flag = True
+    if flag == False:
+        return render(request, 'index.html')
+    else:
+        return render(request, 'main.html', {'data': flag})
+
+def main(request):
+    return render(request, 'main.html')
+
+def museums(request):
+    return render(request, 'museums.html')
